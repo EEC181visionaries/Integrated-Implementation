299311cd9289e4300851b0bdc0b5c14e99dd727a
MaJessica9 <jamma@ucdavis.edu>
2015-05-16 00:22:46 -0700

    Update ROI.v

--- V/ROI.v
+++ V/ROI.v
@@ -51,75 +51,140 @@ module ROI(
 				iRST
 				);
 
-input 	iStart;
-input 	iDone;
+input 		iStart;
+input 		iDone;
 input		iDATA;
 input		iDVAL;
 input		iCLK;
 input		iRST;
 output	reg	[7:0] oSize;
 
+// SDRAM Storage of Image
 reg  target_image [239:0][319:0];
+
+// Index for Storing Image
 reg [7:0] row_index = 0;
 reg [7:0] col_index = 0;
 reg finished = 0;
 integer r;
 integer c;
 
+// Calculating Start and End Columns
+reg [7:0] sum[319:0] = 0;
+reg [7:0] prev_sum = 0;
+reg [7:0] start_col = 0;
+reg [7:0] end_col = 0;
+reg 		calcStart = 0;
+reg 		calcDone = 0;
+reg 		sumEnable = 0;
+
 
 always @(posedge iCLK or negedge iRST)
 begin
-if (!iRST)
-	begin
+
+	if (!iRST)
+	begin // if reset
 /*			for (r = 0; r < 240; r = r+1)
 			begin
 				for (c = 0; c < 320; c = c+1)
 					target_image[r][c] <= 0;
 			end*/
-	end
-else
-begin
-	if (iStart)
-		begin
-		if (finished)
-			finished <= 0;
-		if (iDVAL)
-			begin
-			
-				target_image[row_index][col_index] <= iDATA;
-			
-			end // end of if val
-		end // end of if start
-	else
-	begin
-		if (iDone)
-		begin
-			oSize <= row_index;
-			finished <= 1;
-		end
-	end
-end // end of else
-end // end of always@
+	end // if reset
 
+	else
+	begin // if not reset
+		
+		if (iStart)
+		begin
+
+			if (finished)
+				finished <= 0;
+
+			if (iDVAL)
+			begin
+				target_image[row_index][col_index] <= iDATA;
+				sum[col_index] <= iDATA;
+			end // if valid
+
+		end // if start
+
+		else if (!iStart)
+		begin // if not start
+
+			if ((sum[0] != 0) && (calcDone == 0))
+			begin
+				calcStart = 1;
+			end
+
+			if (iDone)
+			begin
+				oSize <= row_index;
+				finished <= 1;
+			end // if done
+
+		end // if not start
+
+	end // not reset
+
+end // at clk or rst
+
+
+// Calculate Start and End Columns
+always @(calcStart)
+begin
+
+	for (r = 0; r < 319; r = r + 1)
+	begin
+
+		if ((sum[r] < (.75 * prev_sum)) && (end_col == 0))
+		begin
+			start_col = r;
+		end
+
+		else if ((sum[r] > (1.50 * prev_sum)) && (start_col != 0))
+		begin
+			end_col = r;
+		end
+
+		prev_sum = sum[r];
+	end
+
+end // Calculate Start and End Column
+
+
+// Set index for matrix
 always @(negedge iCLK)// or negedge iRST)
 begin
+
 	if (iStart)
+	begin
+
+		if (iDVAL)
 		begin
-			if (iDVAL)
-				begin
-					col_index <= col_index + 1'b1;
-				end
-			if (col_index > 319)
-				begin
-					col_index <= 0;
-					row_index <= row_index + 1'b1;
-				end
+			col_index <= col_index + 1'b1;
 		end
+
+		if (col_index > 319)
+		begin
+			col_index <= 0;
+			row_index <= row_index + 1'b1;
+		end
+
+	end
+
 	if (finished)	// reset registers for new round;
-			begin
-				row_index <= 0;
-				col_index <= 0;
-			end
+	begin
+		row_index <= 0;
+		col_index <= 0;
+	end
+
 end
 
 endmodule
+
+
+
+
+
+
+
