1b7270cd97faea5b0a5a9b7d23b173054887a2f1
ChanPhilip77 <Zahl506@gmail.com>
2015-05-24 22:52:18 -0700

    Update bit_queuer.v

--- Image-Loading/bit_queuer.v
+++ Image-Loading/bit_queuer.v
@@ -3,16 +3,22 @@ module BitQueuer(
 				oData,
 				oRD_CLK,
 				oRD_RST,
+				oCounter,
+				oRead_req,
 				iData,
 				iCLK,
 				iRST,
 				iHPS_CLK,
-				iHPS_RST
+				iHPS_RST,
+				oflag
 				);
 
 output [31:0] oData;
 output oRD_CLK;
 output oRD_RST;
+output [6:0] oCounter;
+output oflag;
+output oRead_req;
 input iData;
 input iCLK;
 input iRST;
@@ -23,13 +29,16 @@ reg [31:0] out_data = 0;
 reg read_clk;
 reg read_reset;
 reg [6:0] counter = 0;
-reg wait_for_HPS = 0;
+reg wait_for_HPS = 1;
+reg counter_flag = 0;
+reg request_read = 1;
 
 assign oData = out_data;
 assign oRD_CLK = read_clk;
 assign oRD_RST = read_reset;
-
-
+assign oCounter = counter;
+assign oflag = counter_flag;
+assign oRead_req = request_read;
 
 
 always @(posedge iCLK or negedge iRST)
@@ -42,6 +51,7 @@ begin
 		read_clk <= 0;
 		wait_for_HPS <= 0;
 		read_reset <= 0;
+		counter_flag <= 0;
 	end
 	else
 	begin
@@ -49,6 +59,7 @@ begin
 		if (iHPS_CLK)
 		begin
 			wait_for_HPS <= 0;
+			counter <= 0;
 		end
 		else
 		begin
@@ -57,11 +68,13 @@ begin
 				read_clk <= read_clk + 1'b1;
 				if (counter[6:1] > 30)	// if currently 31, set to 0
 				begin
-					counter <= 0;
 					wait_for_HPS <= 1;
 				end
 				else
+				begin
 					counter <= counter + 1'b1;
+					counter_flag <= 1;
+				end
 			end
 		end
 	end
